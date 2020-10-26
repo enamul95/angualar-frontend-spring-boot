d246a396c816af102e1f566535e20da55d86cf43
Israt <ehaque95@gmail.com>
2020-10-26 16:51:40 +0600

    add todo list & navigation

---
+++ src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+
+  todos=[
+    {id:1, description:'Learn to dance'},
+    {id:2, description:'Become an Expert in angular'},
+    {id:3, description:'Visit Bangladesh'},
+  ]
+  // todo = {
+  //   id:1,
+  //   description:'Learn to dance'
+  // }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
